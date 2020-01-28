9e75812059dc0bb8b52b74cb58f3ece47cba3c92
Esha <60232135+Esha98@users.noreply.github.com>
2020-01-28 23:05:50 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,254 @@
+
+;(function () {
+	'use strict';
+	/* 1. Windows on Load
+	====================*/
+	$(window).on('load', function() {
+		$('.loader').delay(2500).fadeOut('slow');
+		var $grid = $('.grid').masonry({
+			itemSelector: '.grid-item',
+			percentPosition: true,
+			columnWidth: '.grid-sizer'
+		});
+	});
+
+	/* 2. Windows on Scroll
+	====================*/
+	var winScrollTop = 0;
+	$(window).on('scroll', function() {
+		var nav = $('#navbar');
+		var top = 200;
+		if ($(window).scrollTop() >= top) {
+			nav.addClass('onscroll');
+		} else {
+			nav.removeClass('onscroll');
+		}
+		winScrollTop = $(this).scrollTop();
+		parallax();
+	});
+
+	/* 3. SVG loader
+	====================*/
+	function mycallback(){
+		this.el.classList.add('finish');
+	}
+	Vivus.prototype.myremoveclass = function () {
+		this.el.classList.remove('finish');
+	}
+	var loaderSvg = new Vivus('my-svg', {
+		type: 'sync',
+		duration: 100,
+		file: 'img/loader.svg',
+		start: 'autostart',
+		dashGap: 20,
+		forceRender: false
+	}, mycallback);
+
+	/* 4. Navbar collapse
+	====================*/
+	$('.navbar-nav>li>a').not('.dropdown-toggle').on('click', function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	/* 5. Circle type
+	====================*/
+	if ($('#js-circle-type').length) {
+		new CircleType(document.getElementById('js-circle-type')).radius(384);
+	}
+
+	/* 6. Slick slider
+	====================*/
+	var slider = function() {
+		if ($('.slick-gallery')) {
+			$('.slick-gallery').slick({
+				centerMode: false,
+				dots: false,
+				infinite: true,
+				speed: 300,
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+							dots: true
+						}
+					},
+					{
+						breakpoint: 768,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
+							dots: true
+						}
+					}
+				]
+			});
+		}
+		if ($('.slick-wishes')) {
+			$('.slick-wishes').slick({
+				dots: true,
+				arrows: false
+			});
+		}
+		if ($('.slick-gifts')) {
+			$('.slick-gifts').slick({
+				dots: true,
+				arrows: false,
+				slidesToShow: 5,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+							dots: true
+						}
+					},
+					{
+						breakpoint: 640,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
+						}
+					}
+				]
+			});
+		}
+	}
+	var sliderNum = function() {
+		var $slides = $('.slick-gallery .slick-slide').not('.slick-cloned');
+		var $currentSlide = $('.slick-slide.slick-current').attr('data-slick-index');
+		$('.gallery__slider-current').text(+$currentSlide + 1);
+		$('.gallery__slider-all').text($slides.length);
+	}
+	$('.slick').on('afterChange', sliderNum);
+
+	/* 7. Countdown
+	====================*/
+	var count = new Date("feb 16, 2020 00:00:00").getTime();
+	var x = setInterval(function() {
+		var now = new Date().getTime();
+		var d = count - now;
+
+		var days = Math.floor(d/(1000*60*60*24));
+		var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
+		var minutes = Math.floor((d%(1000*60*60))/(1000*60));
+		var seconds = Math.floor((d%(1000*60))/(1000));
+
+		 document.getElementById("days").innerHTML = days;
+		 document.getElementById("hours").innerHTML = hours;
+		 document.getElementById("minutes").innerHTML = minutes;
+		 document.getElementById("seconds").innerHTML = seconds;
+
+		 if(d <= 0) {
+			 clearInterval(x);
+		 }
+	},1000);
+
+	/* 8. Form
+	====================*/
+	function filledLabels() {
+		var inputFields = $('.control-label').next();
+		inputFields.each(function(){
+			var singleInput = $(this);
+			singleInput.on('focus blur', function(event){
+				checkVal(singleInput);
+			});
+		});
+	}
+	function checkVal(inputField) {
+		if (inputField.val() === '') {
+			if (event.type === "focus") {
+				inputField.prev('.control-label').addClass('filled')
+			} else if (event.type === "blur") {
+				inputField.prev('.control-label').removeClass('filled')
+			}
+		}
+	}
+	function submitForm() {
+		var $form = $('#rsvp-form');
+		$form.submit(function (e) {
+			$form.find('.error-msg').remove();
+			$form.find('input').removeClass('error');
+			var formData = {
+				'name': $form.find('input#inputName').val(),
+				'number': $form.find('input#inputNumber').val()
+			};
+
+			$.ajax({
+				type: 'POST',
+				url: 'form.php',
+				data: formData,
+				dataType: 'json',
+				encode: true,
+			}).done(function(data) {
+				console.log(data);
+				if (data.success) {
+					$('.success-msg').html('');
+					$('.success-msg').html(data.message);
+				} else {
+					if (data.errors.name) {
+						$('#inputName').addClass('error').after('<span class="error-msg">'+data.errors.name+'</span>');
+					} else if (data.errors.number) {
+						$('#inputNumber').addClass('error').after('<span class="error-msg">'+data.errors.number+'</span>');
+					}
+				}
+			});
+			e.preventDefault();
+		});
+	};
+	/* 9. Page scroll
+	====================*/
+	var pageScroll = function() {
+		$('body').on('click touch', '.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	};
+	/* 10. Parallax
+	====================*/
+	$.fn.is_on_screen = function(){
+		var win = $(window);
+		var viewport = {
+			top : win.scrollTop(),
+			left : win.scrollLeft()
+		};
+		//viewport.right = viewport.left + win.width();
+		viewport.bottom = viewport.top + win.height();
+
+		var bounds = this.offset();
+		//bounds.right = bounds.left + this.outerWidth();
+		bounds.bottom = bounds.top + this.outerHeight();
+
+		return (!(viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+	};
+	function parallax() {
+		var scrolled = $(window).scrollTop();
+		$('.parallax').each(function(){
+
+		if ($(this).is_on_screen()) {
+			var firstTop = $(this).offset().top;
+				var moveTop = (firstTop-winScrollTop)*0.2 //speed;
+				$(this).css("transform","translateY("+moveTop+"px)");
+			}
+		});
+	}
+
+	$(function(){
+		slider();
+		sliderNum();
+		countdown();
+		filledLabels();
+		pageScroll();
+		submitForm();
+	});
+
+}());
